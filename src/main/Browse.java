fb2097b7e59e4f59c7f578b94d4c32640b6f35ff
Michael <michael.li.web@gmail.com>
2021-01-16 23:05:52 -0500

    Chinese characters are now displayed
    Had to change font to support Unicode characters

--- src/main/Browse.java
+++ src/main/Browse.java
@@ -5,6 +5,7 @@ import objects.Novel;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+import tools.ButtonStyle;
 import tools.Design;
 
 import javax.imageio.ImageIO;
@@ -22,10 +23,12 @@ public class Browse {
     private JPanel content = new JPanel(), top, center, bot;
 
     private static ArrayList<Novel> list;
-    private Novel novel;
+    private static Novel novel;
 
-    private int novelPerPage = 10;
-    private double scaleFactor = 3/5f;
+    private static int novelPerPage = 20, initial = 20, page = 1;
+    private static double scaleFactor = 3/5f;
+
+    private static int novelWidth, novelHeight, thickness = 4;;
 
     public Browse(JFrame frame) {
         this.frame = frame;
@@ -43,28 +46,54 @@ public class Browse {
     private void setupContent() {
         for(int i=0;i<list.size();i++) {
             novel = list.get(i);
+
+            //novel thumbnail
             JLabel icon = new JLabel();
-            icon.setLayout(null);
-            int novelWidth = (int)(novel.getThumbnailWidth()*scaleFactor);
-            int novelHeight = (int)(novel.getThumbnailHeight()*scaleFactor);
-            System.out.printf("%d | %d %d\n", i, novelWidth, novelHeight);
-            int thickness = 4;
             icon.setIcon(new ImageIcon(novel.getThumbnail().getImage().getScaledInstance(novelWidth, novelHeight, 0)));
             icon.setBounds(50, 50 + i*(novelHeight+50), novelWidth+2*thickness, novelHeight+2*thickness);
-            center.add(icon);
             icon.setBorder(BorderFactory.createLineBorder(Design.screenPop, thickness));
+            center.add(icon);
+
+            //novel title
+            JLabel title = new JLabel("<html>"+novel.getNovelName()+"</html>");
+            title.setForeground(Design.foreground);
+            title.setFont(Design.buttonTextFont.deriveFont(24f));
+            title.setBounds(200, 20 + i*(novelHeight+50), 350, 100);
+            center.add(title);
+
+            //novel author
+            JLabel author = new JLabel(novel.getAuthor());
+            author.setForeground(Design.foreground);
+            author.setFont(Design.buttonTextFont);
+            author.setBounds(200, 100 + i*(novelHeight+50), 350, 50);
+            center.add(author);
         }
+
+        JButton viewMore = new JButton("View More");
+        viewMore.setFont(Design.buttonTextFont.deriveFont(24f));
+        viewMore.setBounds(200, 50 + list.size()*(novelHeight+50), 200, 50);
+        viewMore.setForeground(Design.screenBackground);
+        viewMore.setBackground(Design.novelButtonBackground);
+        viewMore.addMouseListener(new ButtonStyle());
+//        resume.addActionListener(e -> refreshScreen(novel.getLastReadChapter()));
+        center.add(viewMore);
+
 
         //JScrollPane to allow for continuous scrolling of browsing novels
         JScrollPane scroll = new JScrollPane(center);
         scroll.setBorder(BorderFactory.createEmptyBorder());
         scroll.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED);
         scroll.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
-        scroll.getVerticalScrollBar().setUnitIncrement(5);
+        scroll.getVerticalScrollBar().setUnitIncrement(15);
         content.add(scroll);
     }
 
     private void setupPanel() {
+        //need to initialize this value to set center panel height
+        novel = list.get(0);
+        novelWidth = (int)(novel.getThumbnailWidth()*scaleFactor);
+        novelHeight = (int)(novel.getThumbnailHeight()*scaleFactor);
+
         //panel to display the mangas
         content.setBackground(Color.GREEN);
         content.setBounds(0, 0, Design.WIDTH, 1000);
@@ -80,10 +109,8 @@ public class Browse {
         //browsable novels
         center = new JPanel();
         center.setBackground(Design.screenLightBackground);
+        center.setPreferredSize(new Dimension(Design.WIDTH, 150+initial*(novelHeight+50)));
         center.setLayout(null);
-//        center.setSize(Design.WIDTH, 1000);
-//        center.setMinimumSize(new Dimension(Design.WIDTH, 1000));
-        center.setPreferredSize(new Dimension(Design.WIDTH, 5000));
 
         //application dashboard
         bot = new JPanel();
@@ -100,15 +127,15 @@ public class Browse {
     public static void main(String[] args) {
         String url = "https://novelfull.com/most-popular?page=1";
 
-        int count = 10, page = 1;
         list = new ArrayList();
+        int count = initial;
         try {
             while(count>0) {
                 Document doc = Jsoup.connect(url).get();
                 //all the <div> with title information are in a class called "col-xs-7"
                 //to call a class from a div, the dot is used
                 for(Element row:doc.select("div.col-xs-7")) {
-                    if(count<0) break;
+                    if(count<=0) break;
                     //finds the <a> tag that is contained in a <h3> tag
                     String novelLink = row.select("h3 > a").attr("href");
                     String novelName = row.select("h3 > a").text();
